da0e5ad57031c641afe7781c78a878b5923e6055
huozhi <gilesliu12@gmail.com>
2019-11-06 10:23:41 +0800

    fix: support for safari hover state (#1)

--- src/helper.js
+++ src/helper.js
@@ -3,9 +3,17 @@ const isTouchSupported = Boolean(window.TouchEvent)
 const isPointerSupported = Boolean(window.PointerEvent)
 
 const isMouseTypeEvent = e => e.pointerType === 'mouse' || (isMouseSupported && e instanceof window.MouseEvent)
-const isPureMouseTypeEvent = e => e.pointerType === 'mouse' ||
-  (isMouseSupported && e instanceof window.MouseEvent && isPointerSupported && !(e instanceof window.PointerEvent))
 const isTouchTypeEvent = e => e.pointerType === 'touch' || (isTouchSupported && e instanceof window.TouchEvent)
+
+/*
+ * isPureMouseTypeEvent
+ * case 1: pointer event, mouse
+ * case 2: pure mouse event, pointer event not supported (e.g. safari)
+ * case 3: pure mouse event, pointer supported but it's not pointer event instance
+ */
+const isPureMouseTypeEvent = e => e.pointerType === 'mouse' ||
+  (isMouseSupported && e instanceof window.MouseEvent &&
+    (!isPointerSupported || !(e instanceof window.PointerEvent)))
 
 function safeCall(fn) {
   return (...args) => {
