e01956fc6311b2a33899fe18576d70cbae1b14f6
huozhi <inbox@huozhi.im>
2021-01-09 21:29:01 +0800

    types: set event as optional

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 export function fscrub(
   node: HTMLElement,
   handlers: {
-    onStart?(e: Event): void,
-    onMove?(e: Event): void,
-    onEnd?(e: Event): void,
+    onStart?(e?: Event): void,
+    onMove?(e?: Event): void,
+    onEnd?(e?: Event): void,
   },
   options?: {
     hover?: boolean, 
@@ -15,22 +15,22 @@ export function fscrub(
 export function ftap(
   node: HTMLElement, 
   handlers: {
-    onTouchDown?(e: Event): void,
-    onTouchDownCapture?(e: Event): void,
-    onTouchUp?(e: Event): void,
-    onTouchUpCapture?(e: Event): void,
-    onTouchClick?(e: Event): void,
-    onTouchClickCapture?(e: Event): void,
-    onMouseDown?(e: Event): void,
-    onMouseDownCapture?(e: Event): void,
-    onMouseUp?(e: Event): void,
-    onMouseUpCapture?(e: Event): void,
-    onMouseClick?(e: Event): void,
-    onMouseClickCapture?(e: Event): void,
-    onHoverEnter?(e: Event): void,
-    onHoverEnterCapture?(e: Event): void,
-    onHoverLeave?(e: Event): void,
-    onHoverLeaveCapture?(e: Event): void,
+    onTouchDown?(e?: Event): void,
+    onTouchDownCapture?(e?: Event): void,
+    onTouchUp?(e?: Event): void,
+    onTouchUpCapture?(e?: Event): void,
+    onTouchClick?(e?: Event): void,
+    onTouchClickCapture?(e?: Event): void,
+    onMouseDown?(e?: Event): void,
+    onMouseDownCapture?(e?: Event): void,
+    onMouseUp?(e?: Event): void,
+    onMouseUpCapture?(e?: Event): void,
+    onMouseClick?(e?: Event): void,
+    onMouseClickCapture?(e?: Event): void,
+    onHoverEnter?(e?: Event): void,
+    onHoverEnterCapture?(e?: Event): void,
+    onHoverLeave?(e?: Event): void,
+    onHoverLeaveCapture?(e?: Event): void,
   }
 ): () => void
 
