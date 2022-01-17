aaf5234ab32305dcbd61ae04702442a35e8db4cd
Jiachi Liu <inbox@huozhi.im>
2022-01-17 21:08:59 +0100

    docs: use absolute url for tw card

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -16,7 +16,7 @@ function Tabs() {
 
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@huozhi' />
-        <meta name='twitter:image' content={`/fpoint.png`} />
+        <meta name='twitter:image' content={`https://fpoint.vercel.app/fpoint.png`} />
       </Head>
       <style jsx>{`
         nav>.tab:not(:first-child) {
